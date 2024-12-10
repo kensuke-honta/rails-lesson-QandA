959602ab4258d61018abbb30e3e83ef3a15551fe
honta kensuke <jianzibenduo0721@gmail.com>
2024-12-10 15:21:21 +0900

    answersモデル作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,15 @@
 class QuestionsController < ApplicationController
   # 質問の一覧表示
   def index
-  
+    @questions = Question.all
+    # p @questions?
   end
 
   # 質問の詳細ページ表示
   def show
-  
+    # p params
+    @question = Question.find(params[:id])
+    # p @question
   end
 
   # 質問の作成
@@ -19,24 +22,35 @@ class QuestionsController < ApplicationController
     #questionモデルを初期化(引数でquestion_paramsを置くことで値をセットできる)
     @question = Question.new(question_params)
     #questionモデルをDBに保存
-    @question.save
+    if @question.save
     #showへリダイレクト
     redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
+
   end
 
   # 質問の編集
   def edit
-  
+    @question = Question.find(params[:id])
   end
 
   # 質問の更新
   def update
-  
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   # 質問の削除
   def destroy
-  
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to question_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "questions#index"
   resources :questions
   # Defines the root path route ("/")
   # root "articles#index"
