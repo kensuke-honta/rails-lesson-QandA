2b10b4fc6ef1f4fe3ed52a6ed281f55a3317b598
honta kensuke <jianzibenduo0721@gmail.com>
2024-12-10 15:51:32 +0900

    Answersコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "questions#index"
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
